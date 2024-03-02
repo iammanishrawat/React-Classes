f2399c0c13b9792ec93e8c470757902ccc551dcb
Manish rawat <manish.rawat@essencesoftwares.com>
2024-03-02 23:25:10 +0530

    console error fix - key error

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 import { createRoot } from 'react-dom/client';
 import './style.scss';
 
-function Card(props) {
-    const {id, images, title, brand, price} = props;
-  return (
-    <div className="card" key={id}>
-      <img src={images} alt="" />
-      <h3>{title}</h3>
-      <p>{brand}</p>
-      <h4>${price}</h4>
-    </div>
-  );
-}
+// function Card(props) {
+//     const {id, images, title, brand, price} = props;
+//     return (
+//       <div className="card" key={id}>
+//         <img src={images} alt="" />
+//         <h3>{title}</h3>
+//         <p>{brand}</p>
+//         <h4>${price}</h4>
+//       </div>
+//     );
+// }
 
-fetch('https://dummyjson.com/products')
-  .then((res) => res.json())
-  .then((data) => {
-    const container = data.products.map((product) => {
-      return Card(
-       {
-        key: product.id,
-        images: product.thumbnail,
-        price: product.price,
-        title: product.title,
-        brand: product.brand,
-       }
-      );
-    });
-    root.render(<div className="flex-grid">{container}</div>);
-  });
+
+
+// fetch('https://dummyjson.com/products')
+//   .then((res) => res.json())
+//   .then((data) => {
+//     const container = data.products.map((product) => {
+//       return Card(
+//        {
+//         id: product.id,
+//         images: product.thumbnail,
+//         price: product.price,
+//         title: product.title,
+//         brand: product.brand,
+//        }
+//       );
+//     });
+//     root.render(<div className="flex-grid">{container}</div>);
+//   });
 
 const root = createRoot(document.getElementById('root'));
